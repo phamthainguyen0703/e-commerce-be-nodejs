6332c22a0b54c01b30c1f06ef0a9280639e50b79
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-07-06 00:56:54 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "test Compression";
+  return res.status(200).json({
+    message: "Welcome!",
+    message: strCompress.repeat(10000),
+  });
+});
+// handling error
+
+module.exports = app;
