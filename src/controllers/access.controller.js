0c5c9d3be4e104287cce08e5c80c81ad0a4b7417
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-07-14 15:35:48 +0700

    Xử lý success response trong API

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,21 @@
 "use strict";
 
+const { CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.signUp(req.body));
+    // return res.status(200).json({
+    //   message: "success",
+    //   metadata:
+    // })
+    new CREATED({
+      message: "User created successfully",
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 10,
+      },
+    }).send(res);
   };
 }
 
--- src/core/success.response.js
+++ src/core/success.response.js
@@ -1,1 +1,49 @@
 "use strict";
+
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+};
+
+const ReasonStatusCode = {
+  CREATED: "CREATED!",
+  OK: "SUCCESS",
+};
+
+class SuccessResponse {
+  constructor(
+    message,
+    statuscode = StatusCode.OK,
+    reasonStatusCode = ReasonStatusCode.OK,
+    metadata = {}
+  ) {
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statuscode;
+    this.metadata = metadata;
+  }
+
+  send(res, headers = {}) {
+    return res.status(this.status).json(this);
+  }
+}
+class OK extends SuccessResponse {
+  constructor({ message, metadata }) {
+    super({ message, metadata });
+  }
+}
+class CREATED extends SuccessResponse {
+  constructor({
+    options = {},
+    message,
+    statuscode = StatusCode.CREATED,
+    reasonStatusCode = ReasonStatusCode.CREATED,
+    metadata,
+  }) {
+    super({ message, statuscode, reasonStatusCode, metadata });
+    this.options = options;
+  }
+}
+module.exports = {
+  OK,
+  CREATED,
+};
